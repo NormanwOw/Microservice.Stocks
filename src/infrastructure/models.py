d2d1af703dddc4e4f54ba62d02c8366dd61e9935
NormanwOw <normjkeee@vk.com>
2026-01-17 12:04:21 +0500

    ADDED StocksModel and ProductsModel, updated migrations

--- src/infrastructure/models.py
+++ src/infrastructure/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime, timezone
 from typing import Optional
 
-from sqlalchemy import UUID, DateTime
+from sqlalchemy import UUID, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -20,6 +20,23 @@ class CUModel:
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=True, onupdate=lambda: datetime.now(timezone.utc)
     )
+
+
+class ProductsModel(Base, CUModel):
+    __tablename__ = 'products'
+
+    name: Mapped[str] = mapped_column(nullable=False)
+
+
+class StocksModel(Base, CUModel):
+    __tablename__ = 'stocks'
+
+    product_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey('products.id', ondelete='CASCADE'), nullable=False
+    )
+    available: Mapped[int] = mapped_column(nullable=False, default=0)
+    reserved: Mapped[int] = mapped_column(nullable=False, default=0)
+    total: Mapped[int] = mapped_column(nullable=False, default=0)
 
 
 class OutboxModel(Base, CUModel):
