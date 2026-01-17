e221e45c9ab267a4b8e28c5c4a9523bf0715cb3a
NormanwOw <normjkeee@vk.com>
2026-01-17 13:14:07 +0500

    COMPLETED get_products_endpoint full implementation

--- src/application/use_cases/get_products_use_case.py
+++ src/application/use_cases/get_products_use_case.py
@@ -1,10 +1,13 @@
+from src.domain.entities import Product
 from src.infrastructure.uow.interfaces import IUnitOfWork
+from src.presentation.pagination import Pagination
 
 
 class GetProducts:
     def __init__(self, uow: IUnitOfWork):
         self.uow = uow
 
-    async def __call__(self):
+    async def __call__(self, pagination: Pagination) -> list[Product]:
         async with self.uow:
-            pass
+            stocks = await self.uow.stocks.find_available(pagination=pagination)
+            return [stock.to_product() for stock in stocks]
