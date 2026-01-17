9ae6814b6024a1bd6a91853b408ccd3a965470d1
NormanwOw <normjkeee@vk.com>
2026-01-17 13:12:57 +0500

    ADDED relationships into ProductsModel and StocksModel, added indexes

--- src/infrastructure/models.py
+++ src/infrastructure/models.py
@@ -5,8 +5,9 @@ from typing import Optional
 
 from sqlalchemy import UUID, DateTime, Enum, ForeignKey, Numeric
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+from src.domain.entities import Product
 from src.domain.enums import Currency
 
 
@@ -28,12 +29,17 @@ class CUModel:
 class ProductsModel(Base, CUModel):
     __tablename__ = 'products'
 
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False, index=True)
     price: Mapped[Decimal] = mapped_column(Numeric(12, 2), nullable=False)
     currency: Mapped[Currency] = mapped_column(
         Enum(Currency, name='currency_enum', native_enum=True, create_type=True),
         nullable=False,
         default=Currency.USD,
+    )
+    stock: Mapped['StocksModel'] = relationship(
+        back_populates='product',
+        lazy='selectin',
+        uselist=False,
     )
 
 
@@ -41,11 +47,26 @@ class StocksModel(Base, CUModel):
     __tablename__ = 'stocks'
 
     product_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey('products.id', ondelete='CASCADE'), nullable=False
+        ForeignKey('products.id', ondelete='CASCADE'), nullable=False, index=True
     )
-    available: Mapped[int] = mapped_column(nullable=False, default=0)
+    available: Mapped[int] = mapped_column(nullable=False, default=0, index=True)
     reserved: Mapped[int] = mapped_column(nullable=False, default=0)
     total: Mapped[int] = mapped_column(nullable=False, default=0)
+
+    product: Mapped['ProductsModel'] = relationship(
+        back_populates='stock',
+        lazy='selectin',
+        uselist=False,
+    )
+
+    def to_product(self) -> Product:
+        return Product(
+            id=self.product.id,
+            name=self.product.name,
+            quantity=self.available,
+            price=self.product.price,
+            currency=self.product.currency,
+        )
 
 
 class OutboxModel(Base, CUModel):
