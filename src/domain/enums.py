1bc51ef029aea40a3085a342b5e76bf22102d4c0
NormanwOw <normjkeee@vk.com>
2026-01-24 14:35:39 +0500

    ADDED CommandType, EventType adn AggregateType into enums

--- src/domain/enums.py
+++ src/domain/enums.py
@@ -4,3 +4,16 @@ from enum import Enum
 class Currency(str, Enum):
     USD = 'USD'
     EUR = 'EUR'
+
+
+class CommandType(str, Enum):
+    RESERVE_PRODUCTS = 'ReserveProducts'
+
+
+class EventType(str, Enum):
+    PRODUCTS_RESERVED = 'ProductsReserved'
+    RESERVE_FAILED = 'ReserveFailed'
+
+
+class AggregateType(str, Enum):
+    ORDER = 'order'
