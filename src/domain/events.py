6320c0c511dc578fd2993c6b442196f1d3253711
NormanwOw <normjkeee@vk.com>
2026-01-24 14:37:26 +0500

    REFACTORED KafkaMessageRouter

--- src/domain/events.py
+++
@@ -1,5 +0,0 @@
-from src.domain.base import PydanticBase
-
-
-class DomainEvent(PydanticBase):
-    pass
--- src/infrastructure/messaging/kafka_router.py
+++ src/infrastructure/messaging/kafka_router.py
@@ -1,10 +1,12 @@
 import asyncio
 
 from sqlalchemy.exc import IntegrityError
+from tenacity import AsyncRetrying, RetryError, stop_after_attempt, wait_fixed
 
 from src.application.dispatcher import dispatcher
+from src.infrastructure.logger.impl import logger
 from src.infrastructure.messaging.interfaces import IKafkaConsumer
-from src.infrastructure.messaging.messages import EventMessage
+from src.infrastructure.messaging.messages import CommandMessage
 from src.infrastructure.models import ProcessedMessagesModel
 from src.infrastructure.uow.interfaces import IUnitOfWork
 
@@ -19,7 +21,7 @@ class KafkaMessageRouter:
         try:
             while True:
                 async for msg in self.consumer:
-                    message_schema = EventMessage(**msg.value)
+                    message_schema = CommandMessage(**msg.value)
 
                     async with self.uow:
                         try:
@@ -30,14 +32,30 @@ class KafkaMessageRouter:
                             await self.consumer.commit()
                             continue
 
-                        await dispatcher.dispatch(
-                            event_type=message_schema.event_type,
-                            message=msg.value,
-                        )
+                        try:
+                            async for attempt in AsyncRetrying(
+                                stop=stop_after_attempt(3), wait=wait_fixed(2)
+                            ):
+                                with attempt:
+                                    await dispatcher.dispatch(
+                                        uow=self.uow,
+                                        command_type=message_schema.command_type,
+                                        message=msg.value,
+                                    )
+                                    await self.uow.commit()
+                        except RetryError:
+                            logger.error(
+                                f'Error while process message: '
+                                f'{message_schema.message_id} | Message: {msg.value}'
+                            )
+                            await self.uow.rollback()
+                            await self.uow.processed_messages.add(
+                                ProcessedMessagesModel(id=message_schema.message_id)
+                            )
+                            await self.uow.commit()
+                        finally:
+                            await self.consumer.commit()
 
-                        await self.uow.commit()
-                        await self.consumer.commit()
-
-                await asyncio.sleep(0.2)
+                await asyncio.sleep(1)
         finally:
             await self.consumer.stop()
