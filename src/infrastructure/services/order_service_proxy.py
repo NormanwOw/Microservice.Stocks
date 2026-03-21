c267f433dac4a29ed25ecab9ff8e1522a1a6b0fb
NormanwOw <normjkeee@vk.com>
2026-03-21 20:21:03 +0500

    ADDED action_failed instead of reserve_failed and commit_failed

--- src/infrastructure/services/order_service_proxy.py
+++ src/infrastructure/services/order_service_proxy.py
@@ -1,16 +1,16 @@
 from src.application.ports.services import IOrderService
 from src.application.ports.uow import IUnitOfWork
-from src.config import Settings
+from src.config import SERVICE_NAME, Settings
 from src.domain.entities import Product
 from src.domain.enums import EventType
-from src.infrastructure.messaging.messages import ExternalReference
+from src.infrastructure.messaging.messages import ExternalReference, FailedMessage
 from src.infrastructure.models import OutboxModel
 
 
 class OrderServiceProxy(IOrderService):
     def __init__(self, settings: Settings):
         self.topic = settings.SAGA_EVENTS_TOPIC
-        self.producer = 'stocks-service'
+        self.producer = SERVICE_NAME
 
     async def products_reserved(
         self, uow: IUnitOfWork, products: list[Product], external_reference: ExternalReference
@@ -41,26 +41,12 @@ class OrderServiceProxy(IOrderService):
         )
         await uow.outbox.add(for_outbox)
 
-    async def reserve_failed(
-        self, uow: IUnitOfWork, error_message: str, external_reference: ExternalReference
-    ):
+    async def action_failed(self, uow: IUnitOfWork, message: FailedMessage):
         for_outbox = OutboxModel(
-            action=EventType.RESERVE_FAILED,
+            action=message.action,
             topic=self.topic,
-            payload={'error_message': error_message},
-            external_reference=external_reference.to_dict(),
-            producer=self.producer,
-        )
-        await uow.outbox.add(for_outbox)
-
-    async def commit_failed(
-        self, uow: IUnitOfWork, error_message: str, external_reference: ExternalReference
-    ):
-        for_outbox = OutboxModel(
-            action=EventType.COMMIT_FAILED,
-            topic=self.topic,
-            payload={'error_message': error_message},
-            external_reference=external_reference.to_dict(),
+            payload=message.payload.to_dict(),
+            external_reference=message.external_reference.to_dict(),
             producer=self.producer,
         )
         await uow.outbox.add(for_outbox)
